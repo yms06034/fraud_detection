a53fd82b7e63beb77683a4af1e87d55429902314
yms06034 <yms06034@gmail.com>
2023-04-26 19:01:51 +0900

    Doing Generate Log Scripts

--- fraud_detection/payment_producer.py
+++ fraud_detection/payment_producer.py
@@ -1,5 +1,5 @@
 from kafka import KafkaProducer
-import datetime
+import datetime 
 import random
 import time
 import pytz
@@ -9,6 +9,8 @@ TOPICNAME = "payments"
 
 brokers = ["localhost:9091", "localhost:9092", "localhost:9093"]
 producer = KafkaProducer(bootstrap_servers=brokers)
+
+STATUS = [200, 200, 200,200, 200, 200, 200, 200, 200, 200, 200, 200, 206, 302, 304, 404, 500]
 
 def get_timedate():
   utc_now = pytz.utc.localize(datetime.datetime.utcnow())
@@ -23,16 +25,29 @@ def generate_payment_data():
   to = random.choice(["me", "mom", "dad", "friend", "stranger"])
   return payment_type, amount, to
 
+def generate_log_data():
+  ip = '.'.join(map(lambda x:int(random.randrange(1, 255)), range(4)))
+  times = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+  methods = random.choice(["GET", "POST"])
+  status = random.choice(STATUS)
+
+  log_data_total = f"{ip} {times} {methods} {status}"
+
+  return log_data_total
+
 while True:
   d, t = get_timedate()
   payment_type, amount, to = generate_payment_data()
+  log_data_total = generate_log_data()
   new_data = {
     "DATE": d,
     "TIME": t,
     "PAYMENT_TYPE": payment_type,
     "AMOUNT": amount,
-    "TO": to
+    "TO": to,
+    "LOG": log_data_total
   }
+
 
   producer.send(TOPICNAME, json.dumps(new_data).encode("utf-8"))
   print(new_data)
